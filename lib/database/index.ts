33957ccfdbe2dc1fd6a9d870d1c9e29b88e52aa8
Kirubel <kirubeldesalegn17@gmail.com>
2025-03-25 20:39:49 +0300

    web hooks

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -2,10 +2,10 @@ import mongoose from 'mongoose'
 
 const MONGODB_URI = process.env.MONGODB_URI
 
-let cached = (global as any).mongoose || {conn:null, promise:null};
+let cached = (global as any).mongoose || {conn:null, promise:null};     //stores a cached connection in the global object (global.mongoose) to prevent unnecessary reconnections.
 
 export const connectToDatabase = async()=>{ 
-    if (cached.conn) return cached.conn;
+    if (cached.conn) return cached.conn;    //If cached.conn (an existing connection) is available, it is returned immediately to avoid creating a new connection.
 
     if(!MONGODB_URI) throw new Error("MONGODB_URI is missing");
 
